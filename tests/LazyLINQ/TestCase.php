a2f22023eca6e642fb6825546390f183fd8bba37
Alexey Kopytko <alexey@kopytko.com>
2020-01-30 23:52:28 +0900

    Fix laziness test

--- tests/LazyLINQ/TestCase.php
+++ tests/LazyLINQ/TestCase.php
@@ -485,10 +485,7 @@ abstract class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase
         $range = static::range(1, LINQ::LAZY_RANGE_MIN_COUNT);
         $actualUsage = memory_get_usage() - $usage;
 
-        if ($actualUsage > $referenceUsage) {
-            $this->markTestIncomplete("Array found to be using $referenceUsage bytes, where our lazy iterator consumed $actualUsage");
-        }
-
+        $this->assertNotEmpty($array); // Tells optimizer to keep this array in memory
         $this->assertLessThan($referenceUsage, $actualUsage);
 
         $this->assertEquals(array_sum(range(1, LINQ::LAZY_RANGE_MIN_COUNT)), $range->sum());
